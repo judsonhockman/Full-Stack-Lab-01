cf6d8595ff84f30b4c00bc5789720e95121ea829
Judson Hockman <judson.hockman@gmail.com>
2017-05-30 16:30:10 -0400

    first resume save

---
+++ resume.js
@@ -0,0 +1,7 @@
+console.log("Name:"+"Judson Hockman".toUpperCase());
+console.log("Career: Field Service Representative");
+console.log("Description: I am a passionate follower of Christ who works well in fellowship with others. I can also be trusted to work with limited supervision.");
+console.log("Interests:");
+console.log("*Fishing");
+console.log("*Fishing");
+console.log("*Did I mention I like to fish?");
