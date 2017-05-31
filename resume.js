14ee1d9d721c6b67e0e9c9b3af02c8c009e96aa9
Judson Hockman <judson.hockman@gmail.com>
2017-05-30 22:42:42 -0400

    Making Progress

--- resume.js
+++ resume.js
@@ -1,7 +1,76 @@
-console.log("Name:"+"Judson Hockman".toUpperCase());
+console.log("Name:" + "Judson Hockman".toUpperCase());
 console.log("Career: Field Service Representative");
 console.log("Description: I am a passionate follower of Christ who works well in fellowship with others. I can also be trusted to work with limited supervision.");
 console.log("Interests:");
 console.log("*Fishing");
 console.log("*Fishing");
 console.log("*Did I mention I like to fish?");
+
+console.log("My Previous Experience:");
+
+function displayPosition(jobTitle, companyName, description) {
+    console.log("Stryker Mechanic");
+    console.log("at");
+    console.log("General Dynamics");
+    console.log("-");
+    console.log("Repaired combat-damaged vehicles");
+}
+ displayPosition();
+
+function displayPosition(jobTitle, companyName, description) {
+    console.log("Astronaut");
+    console.log("at");
+    console.log("NASA");
+    console.log("-");
+    console.log("Tested flight suits")
+}
+displayPosition();
+
+function displayPosition(jobTitle, companyName, description) {
+    console.log("Disc Jockey");
+    console.log("at");
+    console.log("U.S. Talent");
+    console.log("-");
+    console.log("Played dance music in night clubs");
+}
+displayPosition();
+
+    console.log("My skills:");
+    console.log("BAM: I can twist to anything");
+    console.log("Change a cross-over tube on a Cat C-7 blindfolded");
+    console.log("BAM: Taught myself how to throw right-handed");
+
+// var cool = "Bam";
+// function displaySkill(name, cool) {
+// if (cool) {
+// console.log("BAM:");
+// } else {
+// }
+// }
+
+// displaySkill();
+
+
+
+// console.log(job title + company name + Description)}
+// use if else to put BAM true or false in front of a skill
+
+
+// function announceWeather(temp, status) {
+//     console.log('The temperature is:');
+//     console.log(temp);
+//     console.log('The sky is:');
+//     console.log(status);
+// }
+// console.log('Hello World!');
+// announceWeather('at', '-');
+// announceWeather(8, 'snowy');
+// // Write your function below. 
+// Don't forget to call your function!
+// var sleepCheck = function (numhours) {
+//  if (numhours >= 8) 
+//   { return "You're getting plenty of sleep! Maybe even too much!";}
+// else 
+//   { return "Get some more shut eye!";}
+// };
+// sleepCheck(10);
